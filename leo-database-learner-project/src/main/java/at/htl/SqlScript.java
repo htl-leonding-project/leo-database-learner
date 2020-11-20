e8e5ae511c7a074cebaa600e0be7df51b7743701
turnerisabel <turner.isabel03@gmail.com>
2020-11-20 14:50:06 +0100

    implemented attributes in SqlScript #8

--- leo-database-learner-project/src/main/java/at/htl/SqlScript.java
+++ leo-database-learner-project/src/main/java/at/htl/SqlScript.java
@@ -1,8 +1,21 @@
 package at.htl;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.sql.Blob;
 
 @Entity
 public class SqlScript {
+
+    @Id //@GeneratedValue(generator = GenerationType.TABLE)
+    Long id;
+
+    //private DataModel dataModel;
+    private String createScript;
+    private String dropScript;
+    private String insertScript;
+
 
 }

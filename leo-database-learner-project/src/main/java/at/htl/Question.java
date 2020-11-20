abc7f86c3bbffd17f08f22da0658849f9ce4b022
turnerisabel <turner.isabel03@gmail.com>
2020-11-20 15:13:15 +0100

    implemented attributes in Question.java #8

--- leo-database-learner-project/src/main/java/at/htl/Question.java
+++ leo-database-learner-project/src/main/java/at/htl/Question.java
@@ -1,8 +1,24 @@
 package at.htl;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.sql.DataSource;
+import java.time.LocalDate;
 
 @Entity
 public class Question {
+
+    @Id //@GeneratedValue(generator = GenerationType.TABLE)
+    Long id;
+
+    private String text;
+    private String sql;
+    private int points;
+    private LocalDate localDate;
+    private DataModel dataModel;
+    private Person owner;
+
 
 }

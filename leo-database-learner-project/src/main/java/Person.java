e55036161c2e133ec3bfc49ddecc420b5134655d
turnerisabel <turner.isabel03@gmail.com>
2020-11-20 14:24:30 +0100

    implemented Getter and Setter in Person #8

--- leo-database-learner-project/src/main/java/Person.java
+++ leo-database-learner-project/src/main/java/Person.java
@@ -18,4 +18,36 @@ public class Person {
         this.lastName = lastName;
         this.role = role;
     }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public Role getRole() {
+        return role;
+    }
+
+    public void setRole(Role role) {
+        this.role = role;
+    }
 }

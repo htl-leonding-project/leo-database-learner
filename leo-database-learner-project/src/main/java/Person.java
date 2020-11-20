e7935764e7874abcd599cb3fef8746bebb361a2a
turnerisabel <turner.isabel03@gmail.com>
2020-11-20 14:20:37 +0100

    implemented attributes in Person #8

--- leo-database-learner-project/src/main/java/Person.java
+++ leo-database-learner-project/src/main/java/Person.java
@@ -1,6 +1,12 @@
+import at.htl.Role;
+
 import javax.persistence.Entity;
 
 @Entity
 public class Person {
+    private int id;
+    private String firstName;
+    private String lastName;
+    private Role role;
 
 }

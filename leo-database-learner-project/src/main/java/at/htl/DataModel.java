3e6e8695c770b039da721fbe4066d5a19803b90b
turnerisabel <turner.isabel03@gmail.com>
2020-11-20 15:00:28 +0100

    implemented attributes in DataModel #8

--- leo-database-learner-project/src/main/java/at/htl/DataModel.java
+++ leo-database-learner-project/src/main/java/at/htl/DataModel.java
@@ -1,8 +1,22 @@
 package at.htl;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.sql.Blob;
 
 @Entity
 public class DataModel {
+
+    @Id //@GeneratedValue(generator = GenerationType.TABLE)
+    Long id;
+
+    private String name;
+    //private Person owner;
+    private String comment;
+    private Blob erd;
+
+
 
 }

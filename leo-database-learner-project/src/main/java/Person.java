35276e5fe00719a164784a540167575183f087a8
turnerisabel <turner.isabel03@gmail.com>
2020-11-20 14:21:32 +0100

    implemented constructor in Person #8

--- leo-database-learner-project/src/main/java/Person.java
+++ leo-database-learner-project/src/main/java/Person.java
@@ -9,4 +9,13 @@ public class Person {
     private String lastName;
     private Role role;
 
+    public Person() {
+    }
+
+    public Person(int id, String firstName, String lastName, Role role) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.role = role;
+    }
 }

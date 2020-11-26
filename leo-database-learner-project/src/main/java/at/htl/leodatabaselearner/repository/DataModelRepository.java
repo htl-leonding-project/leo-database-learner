5f706a692ebba698ecb926870e4a1acf6b56b531
steigersdorfermarah <madschi.steigersdorfer@gmx.at>
2020-11-26 11:30:30 +0100

    created DataModelRepository #9

---
+++ leo-database-learner-project/src/main/java/at/htl/leodatabaselearner/repository/DataModelRepository.java
@@ -0,0 +1,16 @@
+package at.htl.leodatabaselearner.repository;
+
+
+import at.htl.leodatabaselearner.entity.DataModel;
+import at.htl.leodatabaselearner.entity.Person;
+import at.htl.leodatabaselearner.entity.Question;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
+import javax.persistence.EntityManager;
+import java.util.List;
+
+@ApplicationScoped
+public class DataModelRepository {
+
+}

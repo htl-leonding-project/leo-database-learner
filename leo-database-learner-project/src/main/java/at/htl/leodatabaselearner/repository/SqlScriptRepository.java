a119c85a630476bc6c07d3faec7282cc8efa381c
steigersdorfermarah <madschi.steigersdorfer@gmx.at>
2020-11-26 20:03:42 +0100

    created SqlScriptRepository #9

---
+++ leo-database-learner-project/src/main/java/at/htl/leodatabaselearner/repository/SqlScriptRepository.java
@@ -0,0 +1,17 @@
+package at.htl.leodatabaselearner.repository;
+
+import at.htl.leodatabaselearner.entity.DataModel;
+import at.htl.leodatabaselearner.entity.SqlScript;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
+import javax.persistence.EntityManager;
+import java.util.List;
+
+@ApplicationScoped
+public class SqlScriptRepository {
+
+    @Inject
+    EntityManager em;
+
+}

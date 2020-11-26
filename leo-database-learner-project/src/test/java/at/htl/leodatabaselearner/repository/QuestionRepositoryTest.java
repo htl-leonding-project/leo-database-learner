9c397e3848e7f36b042757e9d018853f37f1a521
steigersdorfermarah <madschi.steigersdorfer@gmx.at>
2020-11-26 19:29:59 +0100

    created QuestionRepositoryTest #10

---
+++ leo-database-learner-project/src/test/java/at/htl/leodatabaselearner/repository/QuestionRepositoryTest.java
@@ -0,0 +1,54 @@
+package at.htl.leodatabaselearner.repository;
+
+import at.htl.leodatabaselearner.entity.DataModel;
+import at.htl.leodatabaselearner.entity.Person;
+import at.htl.leodatabaselearner.entity.Question;
+import at.htl.leodatabaselearner.entity.Role;
+import io.quarkus.test.junit.QuarkusTest;
+import org.assertj.db.type.Table;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
+import org.postgresql.ds.PGSimpleDataSource;
+
+import javax.inject.Inject;
+import javax.sql.DataSource;
+import javax.transaction.*;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.db.output.Outputs.output;
+import static org.junit.jupiter.api.Assertions.*;
+
+@QuarkusTest
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+class QuestionRepositoryTest {
+
+    @Inject
+    QuestionRepository questionRepo;
+
+    @Inject
+    PersonRepository personRepo;
+
+    @Inject
+    DataModelRepository dataModelRepo;
+
+    @Inject
+    UserTransaction tx;
+
+
+    static final String DATABASE = "db";
+    static final String USERNAME = "app";
+    static final String PASSWORD = "app";
+    public static final String URL = "jdbc:postgresql://localhost:5432/db";
+
+    public static DataSource getDataSource() {
+        PGSimpleDataSource dataSource = new PGSimpleDataSource();
+        dataSource.setDatabaseName(DATABASE);
+        dataSource.setUser(USERNAME);
+        dataSource.setPassword(PASSWORD);
+        return dataSource;
+    }
+}

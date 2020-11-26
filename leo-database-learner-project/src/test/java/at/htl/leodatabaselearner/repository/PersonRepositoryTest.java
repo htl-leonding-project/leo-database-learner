ede3ec702e29b96ccfe9a5958d91841fc1d6e2ce
steigersdorfermarah <madschi.steigersdorfer@gmx.at>
2020-11-26 10:24:26 +0100

    updated PersonRepositoryTest - implemented addPerson #10

--- leo-database-learner-project/src/test/java/at/htl/leodatabaselearner/repository/PersonRepositoryTest.java
+++ leo-database-learner-project/src/test/java/at/htl/leodatabaselearner/repository/PersonRepositoryTest.java
@@ -2,12 +2,63 @@ package at.htl.leodatabaselearner.repository;
 
 import at.htl.leodatabaselearner.entity.Person;
 import at.htl.leodatabaselearner.entity.Role;
+import io.quarkus.test.junit.QuarkusTest;
+import org.assertj.db.type.Table;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.postgresql.ds.PGSimpleDataSource;
+
+
+import javax.inject.Inject;
+import javax.sql.DataSource;
+import javax.transaction.*;
+
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.db.output.Outputs.output;
 
-class PersonRepositoryTest {
+
+@QuarkusTest
+public class PersonRepositoryTest {
+
+    @Inject
+    PersonRepository repo;
+
+    @Inject
+    UserTransaction tx;
+
+    @Test
+    public void addPerson() throws SystemException, NotSupportedException, HeuristicRollbackException, HeuristicMixedException, RollbackException {
+
+        Person person = new Person("Susi","Snow", Role.ADMIN);
+        Table personTable = new Table(getDataSource(), "person");
+        output(personTable).toConsole();
+
+        tx.begin();
+        repo.addPerson(person);
+        tx.commit();
+
+        personTable = new Table(getDataSource(), "person");
+        output(personTable).toConsole();
+
+        assertThat(person.getId()).isEqualTo(3L);
+
+    }
+
+
+    static final String DATABASE = "db";
+    static final String USERNAME = "app";
+    static final String PASSWORD = "app";
+    public static final String URL = "jdbc:postgresql://localhost:5432/db";
+
+    public static DataSource getDataSource() {
+        PGSimpleDataSource dataSource = new PGSimpleDataSource();
+        dataSource.setDatabaseName(DATABASE);
+        dataSource.setUser(USERNAME);
+        dataSource.setPassword(PASSWORD);
+        return dataSource;
+    }
 
 }

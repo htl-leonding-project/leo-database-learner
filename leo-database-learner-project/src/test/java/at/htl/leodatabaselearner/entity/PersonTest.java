d743bd6c643230fd021bed438fa1d70b8236a94d
turnerisabel <turner.isabel03@gmail.com>
2020-11-27 09:08:23 +0100

    implemented testToString.java in PersonTest #13

--- leo-database-learner-project/src/test/java/at/htl/leodatabaselearner/entity/PersonTest.java
+++ leo-database-learner-project/src/test/java/at/htl/leodatabaselearner/entity/PersonTest.java
@@ -1,7 +1,20 @@
 package at.htl.leodatabaselearner.entity;
 
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
 
 class PersonTest {
+    @Test
+    void testToString() {
+        Person person = new Person("Max", "Muster", Role.INSTRUCTOR);
+        String testString = person.toString();
+        String rightString = "Person mit der " + "id"
+                + person.getId() + "namens "
+                + person.getFirstName() + "" + person.getLastName() + "hat die Rolle "
+                + person.getRole() + ".\n";
 
+
+
+    }
 }
+
+

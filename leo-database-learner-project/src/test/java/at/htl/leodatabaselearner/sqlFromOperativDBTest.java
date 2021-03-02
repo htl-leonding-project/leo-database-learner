7e8ac3b240a301d0f41f309d1862f2049bad718d
primetzvan <vaniprimetz@gmail.com>
2021-03-02 22:28:59 +0100

    added t0020_getQuestion_pictureSQLOnTerminal() in sqlFromOperativDBTest.java #28

--- leo-database-learner-project/src/test/java/at/htl/leodatabaselearner/sqlFromOperativDBTest.java
+++ leo-database-learner-project/src/test/java/at/htl/leodatabaselearner/sqlFromOperativDBTest.java
@@ -1,4 +1,5 @@
 package at.htl.leodatabaselearner;
+
 import org.junit.jupiter.api.*;
 
 import java.sql.*;
@@ -7,27 +8,49 @@ import java.util.Properties;
 @TestMethodOrder(MethodOrderer.Alphanumeric.class)
 public class sqlFromOperativDBTest {
 
-    @Test
-    @Order(1)
-    void t0010_getEmp_pictureOnTerminal() throws SQLException {
+  @Test
+  @Order(1)
+  void t0010_getEmp_pictureOnTerminal() throws SQLException {
 
-      String url = "jdbc:postgresql://localhost:5433/postgres";
-      Properties props = new Properties();
-      props.setProperty("user","postgres");
-      props.setProperty("password","app");
-      Connection connection = DriverManager.getConnection(url, props);
+    String url = "jdbc:postgresql://localhost:5433/postgres";
+    Properties props = new Properties();
+    props.setProperty("user", "postgres");
+    props.setProperty("password", "app");
+    Connection connection = DriverManager.getConnection(url, props);
 
 
-      StringBuilder sb = new StringBuilder();
-           PreparedStatement ps = connection.prepareStatement(" SELECT * FROM emp");
-           ResultSet rs = ps.executeQuery();
-        while (rs.next()) {
-          sb.append(rs.getString("ename")).append("\n");
-        }
-
-      System.out.println(sb.toString());
-
+    StringBuilder sb = new StringBuilder();
+    PreparedStatement ps = connection.prepareStatement(" SELECT * FROM emp");
+    ResultSet rs = ps.executeQuery();
+    while (rs.next()) {
+      sb.append(rs.getString("ename")).append("\n");
     }
+
+    System.out.println(sb.toString());
+
+  }
+
+  @Test
+  @Order(2)
+  void t0020_getQuestion_pictureSQLOnTerminal() throws SQLException {
+
+    String url = "jdbc:postgresql://localhost:5432/postgres";
+    Properties props = new Properties();
+    props.setProperty("user", "postgres");
+    props.setProperty("password", "app");
+    Connection connection = DriverManager.getConnection(url, props);
+
+
+    StringBuilder sb = new StringBuilder();
+    PreparedStatement ps = connection.prepareStatement(" SELECT * FROM question");
+    ResultSet rs = ps.executeQuery();
+    while (rs.next()) {
+      sb.append(rs.getString("sql")).append("\n");
+    }
+
+    System.out.println(sb.toString());
+
+  }
 
 
 }

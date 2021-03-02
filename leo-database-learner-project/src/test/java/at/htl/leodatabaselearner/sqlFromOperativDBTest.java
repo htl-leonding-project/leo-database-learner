b2c52394e4704dbc42abb29c9ae2888f8ad31ff4
primetzvan <vaniprimetz@gmail.com>
2021-03-02 22:32:09 +0100

    added t0030_pictureResultsetSize() in sqlFromOperativDBTest.java #28

--- leo-database-learner-project/src/test/java/at/htl/leodatabaselearner/sqlFromOperativDBTest.java
+++ leo-database-learner-project/src/test/java/at/htl/leodatabaselearner/sqlFromOperativDBTest.java
@@ -52,5 +52,47 @@ public class sqlFromOperativDBTest {
 
   }
 
+  @Test
+  @Order(3)
+  void t0030_pictureResultsetSize() throws SQLException {
+
+    String url = "jdbc:postgresql://localhost:5432/postgres";
+    Properties props = new Properties();
+    props.setProperty("user", "postgres");
+    props.setProperty("password", "app");
+    Connection connection = DriverManager.getConnection(url, props);
+
+
+    StringBuilder sb = new StringBuilder();
+    PreparedStatement ps = connection.prepareStatement(" SELECT * FROM question");
+    ResultSet rs = ps.executeQuery();
+    while (rs.next()) {
+      if (rs.getInt("id") == 4) {
+        sb.append(rs.getString("sql")).append("\n");
+      }
+    }
+
+    String sql = sb.toString();
+
+    System.out.println(sql);
+
+    url = "jdbc:postgresql://localhost:5433/postgres";
+    props = new Properties();
+    props.setProperty("user", "postgres");
+    props.setProperty("password", "app");
+    connection = DriverManager.getConnection(url, props);
+
+    ps = connection.prepareStatement(sql);
+    rs = ps.executeQuery();
+
+    int size = 0;
+    while (rs.next()) {
+      size++;
+    }
+
+    System.out.println(size);
+
+  }
+
 
 }

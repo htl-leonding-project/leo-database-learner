36bace0d762c66e26a1dcb0d8151f3d1778213ea
primetzvan <vaniprimetz@gmail.com>
2021-03-02 22:33:10 +0100

    added t0040_pictureResultsetTable() in sqlFromOperativDBTest.java #28

--- leo-database-learner-project/src/test/java/at/htl/leodatabaselearner/sqlFromOperativDBTest.java
+++ leo-database-learner-project/src/test/java/at/htl/leodatabaselearner/sqlFromOperativDBTest.java
@@ -94,5 +94,57 @@ public class sqlFromOperativDBTest {
 
   }
 
+  @Test
+  @Order(4)
+  void t0040_pictureResultsetTable() throws SQLException {
+
+    String url = "jdbc:postgresql://localhost:5432/postgres";
+    Properties props = new Properties();
+    props.setProperty("user", "postgres");
+    props.setProperty("password", "app");
+    Connection connection = DriverManager.getConnection(url, props);
+
+
+    StringBuilder sb = new StringBuilder();
+    PreparedStatement ps = connection.prepareStatement(" SELECT * FROM question");
+    ResultSet rs = ps.executeQuery();
+    while (rs.next()) {
+      if (rs.getInt("id") == 4) {
+        sb.append(rs.getString("sql")).append("\n");
+      }
+    }
+
+    String sql = sb.toString();
+
+    System.out.println(sql);
+
+    url = "jdbc:postgresql://localhost:5433/postgres";
+    props = new Properties();
+    props.setProperty("user", "postgres");
+    props.setProperty("password", "app");
+    connection = DriverManager.getConnection(url, props);
+
+    ps = connection.prepareStatement(sql);
+    rs = ps.executeQuery();
+
+    ResultSetMetaData rsmd = rs.getMetaData();
+    int columnsNumber = rsmd.getColumnCount();
+
+    for (int j = 1; j <= columnsNumber; j++) {
+      System.out.print(rsmd.getColumnName(j) + " ");
+    }
+
+    System.out.println("\n");
+
+    while (rs.next()) {
+      for (int i = 1; i <= columnsNumber; i++) {
+        if (i > 1) System.out.print(",  ");
+        String columnValue = rs.getString(i);
+        System.out.print(columnValue);
+      }
+      System.out.println("");
+    }
+  }
+
 
 }

a5dea504c95b5b552cef7f00f9c637f57c3f5b4d
primetzvan <vaniprimetz@gmail.com>
2021-03-12 11:24:25 +0100

    added test t0020_compareResultsetSizeColumns() #29

--- leo-database-learner-project/src/test/java/at/htl/leodatabaselearner/compareResultsets.java
+++ leo-database-learner-project/src/test/java/at/htl/leodatabaselearner/compareResultsets.java
@@ -58,13 +58,64 @@ public class compareResultsets {
 
     sql = "Select * from salgrade";
 
-    Statement  statement = connection.createStatement();
+    Statement statement = connection.createStatement();
     rs = statement.executeQuery(sql);
 
     int sizeST = 0;
     while (rs.next()) {
       sizeST++;
     }
+
+    assertThat(sizeOP).isEqualTo(sizeST);
+
+  }
+
+  @Test
+  @Order(2)
+  void t0020_compareResultsetSizeColumns() throws SQLException {
+
+    String url = "jdbc:postgresql://localhost:5432/postgres";
+    Properties props = new Properties();
+    props.setProperty("user", "postgres");
+    props.setProperty("password", "app");
+    Connection connection = DriverManager.getConnection(url, props);
+
+
+    StringBuilder sb = new StringBuilder();
+    PreparedStatement ps = connection.prepareStatement(" SELECT * FROM question");
+    ResultSet rs = ps.executeQuery();
+    while (rs.next()) {
+      if (rs.getInt("id") == 4) {
+        sb.append(rs.getString("sql")).append("\n");
+      }
+    }
+
+    String sql = sb.toString();
+
+    url = "jdbc:postgresql://localhost:5433/postgres";
+    props = new Properties();
+    props.setProperty("user", "postgres");
+    props.setProperty("password", "app");
+    connection = DriverManager.getConnection(url, props);
+
+    ps = connection.prepareStatement(sql);
+    rs = ps.executeQuery();
+    ResultSetMetaData meta = rs.getMetaData();
+    int sizeOP = meta.getColumnCount();
+
+    url = "jdbc:postgresql://localhost:5433/postgres";
+    props = new Properties();
+    props.setProperty("user", "postgres");
+    props.setProperty("password", "app");
+    connection = DriverManager.getConnection(url, props);
+
+    sql = "Select * from salgrade";
+
+    Statement statement = connection.createStatement();
+    rs = statement.executeQuery(sql);
+    meta = rs.getMetaData();
+
+    int sizeST = meta.getColumnCount();
 
     assertThat(sizeOP).isEqualTo(sizeST);
 

644ba3d72dd2019a77671519ba7503918630ac01
Marah Steigersdorfer <madschi.steigersdorfer@gmx.at>
2021-03-25 17:10:14 +0100

    updated compareResultsets - added t0030_compareResultsetColumnLabel() #29

--- leo-database-learner-project/src/test/java/at/htl/leodatabaselearner/compareResultsets.java
+++ leo-database-learner-project/src/test/java/at/htl/leodatabaselearner/compareResultsets.java
@@ -175,6 +175,68 @@ public class compareResultsets {
 
   }
 
+  @Test
+  @Order(4)
+  void t0040_compareResultsetColumnLabel() throws SQLException {
+    String url = "jdbc:postgresql://localhost:5432/postgres";
+    Properties props = new Properties();
+    props.setProperty("user", "postgres");
+    props.setProperty("password", "app");
+    Connection connection = DriverManager.getConnection(url, props);
 
+
+    StringBuilder sb = new StringBuilder();
+    PreparedStatement ps = connection.prepareStatement(" SELECT * FROM question");
+    ResultSet rs = ps.executeQuery();
+
+
+    while (rs.next()) {
+      if (rs.getInt("id") == 4) {
+        sb.append(rs.getString("sql")).append("\n");
+      }
+    }
+
+    String sql = sb.toString();
+
+    url = "jdbc:postgresql://localhost:5433/postgres";
+    props = new Properties();
+    props.setProperty("user", "postgres");
+    props.setProperty("password", "app");
+    connection = DriverManager.getConnection(url, props);
+
+    ps = connection.prepareStatement(sql);
+    rs = ps.executeQuery();
+    ResultSetMetaData metaOP = rs.getMetaData();
+    int columnCountOP = metaOP.getColumnCount();
+
+    sql = "Select * from salgrade";
+
+    Statement statement = connection.createStatement();
+    rs = statement.executeQuery(sql);
+    ResultSetMetaData metaST = rs.getMetaData();
+    int columnCountST = metaST.getColumnCount();
+    int sizeST = 0;
+    while (rs.next()){
+      sizeST++;
+    }
+
+    String[] labels = new String[5];
+
+
+    if (columnCountOP == columnCountST){
+      labels = new String[columnCountOP];
+
+      for (int i = 0; i < columnCountST; i++) {
+        if (metaST.getColumnLabel(i+1).equals(metaOP.getColumnLabel(i+1))){
+          labels[i] = metaST.getColumnLabel(i+1);
+        }
+      }
+    }
+
+    String[] correctLabels = {"grade", "losal", "hisal"};
+    //assertThat(columnCountST).isEqualTo(columnCountOP);
+    assertThat(labels).isEqualTo(correctLabels);
+
+  }
 
 }

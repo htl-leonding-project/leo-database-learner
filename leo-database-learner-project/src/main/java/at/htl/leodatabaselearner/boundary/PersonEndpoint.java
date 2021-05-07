86f278f05de79e4c93091fe66166375eed0cdcf9
Marah Steigersdorfer <madschi.steigersdorfer@gmx.at>
2021-05-07 10:45:58 +0200

    updated PersonEndpoint - added addPerson() #38

--- leo-database-learner-project/src/main/java/at/htl/leodatabaselearner/boundary/PersonEndpoint.java
+++ leo-database-learner-project/src/main/java/at/htl/leodatabaselearner/boundary/PersonEndpoint.java
@@ -3,13 +3,16 @@ package at.htl.leodatabaselearner.boundary;
 import at.htl.leodatabaselearner.entity.Person;
 import at.htl.leodatabaselearner.repository.PersonRepository;
 
+import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
+import javax.transaction.Transactional;
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import java.util.List;
 
-@Path("person")
+@Path("/person")
+@ApplicationScoped
 @Produces(MediaType.APPLICATION_JSON)
 @Consumes(MediaType.APPLICATION_JSON)
 public class PersonEndpoint {
@@ -17,14 +20,18 @@ public class PersonEndpoint {
   @Inject
   PersonRepository personRepository;
 
+  @POST
+  @Path("addPerson")
+  @Transactional
+  public void addPerson(Person person){
+    personRepository.addPerson(person);
+  }
+
   @GET
   @Path("/getAll")
   public List<Person> getAllPersons() {
-
     final List<Person> result = personRepository.findAll();
-
     return result;
-
   }
 
 }

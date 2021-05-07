b79093afe6c1272cef625c5e43aebcb4de3e3654
Marah Steigersdorfer <madschi.steigersdorfer@gmx.at>
2021-05-07 10:48:59 +0200

    created SqlScriptEndpoint - added addSqlScript() #38

---
+++ leo-database-learner-project/src/main/java/at/htl/leodatabaselearner/boundary/SqlScriptEndpoint.java
@@ -0,0 +1,30 @@
+package at.htl.leodatabaselearner.boundary;
+
+import at.htl.leodatabaselearner.entity.DataModel;
+import at.htl.leodatabaselearner.entity.SqlScript;
+import at.htl.leodatabaselearner.repository.SqlScriptRepository;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
+import javax.transaction.Transactional;
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+import java.util.List;
+
+@Path("/sqlscript")
+@ApplicationScoped
+@Produces(MediaType.APPLICATION_JSON)
+@Consumes(MediaType.APPLICATION_JSON)
+public class SqlScriptEndpoint {
+
+    @Inject
+    SqlScriptRepository sqlScriptRepository;
+
+    @POST
+    @Path("addSqlScript")
+    @Transactional
+    public void addDataModel(SqlScript sqlScript){
+        sqlScriptRepository.addSqlScript(sqlScript);
+    }
+
+}

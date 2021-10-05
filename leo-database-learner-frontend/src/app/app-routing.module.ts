f28ce7d5f529b4c1816ecc42da3e73042564ac88
ChrissyRaffeiner <christine.raffeiner@gmail.com>
2021-10-05 10:17:27 +0200

    included Sidenav and created new Components #61

--- leo-database-learner-frontend/src/app/app-routing.module.ts
+++ leo-database-learner-frontend/src/app/app-routing.module.ts
@@ -2,11 +2,18 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import {TasksComponent} from './tasks/tasks.component';
 import {TaskDetailComponent} from './task-detail/task-detail.component';
+import { AboutComponent } from './about/about.component';
+import { ImpressumComponent } from './impressum/impressum.component';
+import { ProfilComponent } from './profil/profil.component';
 
 const routes: Routes = [
   { path: '', pathMatch: 'full', redirectTo: 'tasks' },
   { path: 'tasks', component: TasksComponent },
-  { path: 'tasks/:id', component: TaskDetailComponent}
+  { path: 'tasks/:id', component: TaskDetailComponent},
+  {path: 'about', component: AboutComponent},
+  {path: "impressum", component: ImpressumComponent},
+  {path: "profile", component: ProfilComponent},
+  {path: "**", component: TasksComponent}
 ];
 
 @NgModule({
--- leo-database-learner-frontend/src/app/tasks/tasks.component.ts
+++ leo-database-learner-frontend/src/app/tasks/tasks.component.ts
@@ -21,5 +21,11 @@ export class TasksComponent implements OnInit {
   ngOnInit(): void {
     this.questionService.getAllQuestion().subscribe(q => this.tasks = q);
     this.personService.getAllPerson().subscribe(p => this.person = p);
+    
   }
+
+  colorNav(){
+    //document.getElementById("menu").style.backgroundColor="#ebe7e7";
+  }
+
 }

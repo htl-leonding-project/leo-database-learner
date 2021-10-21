de7448694861094a5f15b343dde722c3620fb80b
primetzvan <vaniprimetz@gmail.com>
2021-10-21 16:47:47 +0200

    updated AutoStart.java

--- leo-database-learner-project/src/main/java/at/htl/leodatabaselearner/AutoStart.java
+++ leo-database-learner-project/src/main/java/at/htl/leodatabaselearner/AutoStart.java
@@ -2,10 +2,7 @@ package at.htl.leodatabaselearner;
 
 import at.htl.leodatabaselearner.boundary.ExerciseSqlEndpoint;
 import at.htl.leodatabaselearner.entity.*;
-import at.htl.leodatabaselearner.repository.DataModelRepository;
-import at.htl.leodatabaselearner.repository.PersonRepository;
-import at.htl.leodatabaselearner.repository.QuestionRepository;
-import at.htl.leodatabaselearner.repository.SqlScriptRepository;
+import at.htl.leodatabaselearner.repository.*;
 import io.quarkus.runtime.StartupEvent;
 
 import javax.enterprise.context.ApplicationScoped;
@@ -19,47 +16,61 @@ import java.util.Scanner;
 public class AutoStart {
 
     @Inject
-    PersonRepository personRepository;
+    TeacherRepository teacherRepository;
+
+    @Inject
+    StudentRepository studentRepository;
 
     @Inject
     DataModelRepository dataModelRepository;
 
     @Inject
+    SqlScriptRepository sqlScriptRepository;
+
+    @Inject
+    ExercisePackageRepository exercisePackageRepository;
+
+    @Inject
     QuestionRepository questionRepository;
 
     @Inject
-    SqlScriptRepository sqlScriptRepository;
+    EvaluationRepository evaluationRepository;
+
+    @Inject
+    AssignmentRepository assignmentRepository;
+
 
     @Transactional
     void startup(@Observes StartupEvent event) {
         System.out.println("It works!");
-        //Person p1 = new Person("vani","prim", Role.ADMIN);
-        //Person p2 = new Person("madschi","stei", Role.ADMIN);
+        Teacher t1 = new Teacher("vani","prim","1234");
+        Teacher t2 = new Teacher("madschi","stei","1234");
 
-       // DataModel dataModel = new DataModel("LeodatabaseLearner", p1, "comment1");
+        Student s1 = new Student("Bernd","test","1234","test");
 
-        //Question question = new Question("text01", "Select d from IT160205.DEPT d", 2, dataModel, p2);
-        //Question question1 = new Question("Geben Sie alle Personen sortiert nach dem Vornamen aus.", "Select p from Person p order by p.firstName", 2, dataModel, p2);
-        //Question question2 = new Question("Geben Sie alle Daten der Tabelle Dept sortiert nach der 'Deptnumber' aus.", "Select * from dept d order by d.deptno",2, dataModel, p2);
-        //Question question3 = new Question("Geben Sie die Namen der Tabelle Emp sortiert nach dem 'Empnamen' aus.", "Select e.ename from emp e order by e.ename",2, dataModel, p2);
-        //Question question4 = new Question("Geben Sie alle Daten der Tabelle Salgrade aus.", "Select * from salgrade",1, dataModel, p2);
+        DataModel dataModel = new DataModel("LeodatabaseLearner", t1, "comment1");
 
-/*        SqlScript sqlScript = new SqlScript(dataModel, "CREATE TABLE STUDENT(\n" +
-                "     id number not null,\n" +
-                "     firstName varchar2(255) not null,\n" +
-                "     lastName varchar2(255) not null,\n" +
-                ");",
-                "DROP TABLE STUDENT;",
-                "INSERT INTO STUDENT (id, firstName, lastName) VALUES (1, 'Lisa', 'Mayr')");*/
+        SqlScript sqlScript = new SqlScript(dataModel,"CREATE TABLE DOGSCHOOL(id number not null, name varchar2(255), location varchar2(255), zip number ",
+        "DROP TABLE DOGSCHOOL;",
+        "INSERT INTO DOGSCHOOL(id, name, location, zip) VALUES (1, 'Hundeschule Mayr', 'Leonding', 4060)");
 
-//        personRepository.addPerson(p1);
-//        personRepository.addPerson(p2);
-//        dataModelRepository.addDataModel(dataModel);
-//        questionRepository.addQuestion(question1);
-//        questionRepository.addQuestion(question2);
-//        questionRepository.addQuestion(question3);
-//        questionRepository.addQuestion(question4);
-//        sqlScriptRepository.addSqlScript(sqlScript);
+        ExercisePackage exercisePackage = new ExercisePackage("Datamodel 1",dataModel);
+
+        Question question = new Question("1. Question","text01", "Select d from IT160205.DEPT d", 2,exercisePackage);
+
+        Evaluation evaluation = new Evaluation(10,s1,question);
+
+        Assignment assignment = new Assignment(s1,exercisePackage);
+
+        teacherRepository.addTeacher(t1);
+        teacherRepository.addTeacher(t2);
+        studentRepository.addStudent(s1);
+        dataModelRepository.addDataModel(dataModel);
+        sqlScriptRepository.addSqlScript(sqlScript);
+        exercisePackageRepository.addExercisePackage(exercisePackage);
+        questionRepository.addQuestion(question);
+        evaluationRepository.addEvaluation(evaluation);
+        assignmentRepository.addAssignment(assignment);
 
     }
 }

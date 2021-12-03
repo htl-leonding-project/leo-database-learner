f42975734ab2badf631496dfd05d35a2e3543c28
Marah Steigersdorfer <madschi.steigersdorfer@gmx.at>
2021-12-03 11:05:13 +0100

    created TeacherRepositoryTest - added save() #77

---
+++ leo-database-learner-project/src/test/java/at/htl/leodatabaselearner/repository/TeacherRepositoryTest.java
@@ -0,0 +1,35 @@
+package at.htl.leodatabaselearner.repository;
+
+import at.htl.leodatabaselearner.entity.Teacher;
+import io.quarkus.test.junit.QuarkusTest;
+import org.junit.jupiter.api.Test;
+
+import javax.inject.Inject;
+import javax.transaction.Transactional;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@QuarkusTest
+class TeacherRepositoryTest {
+
+
+
+    @Inject
+    TeacherRepository teacherRepository;
+
+
+    @Test
+    @Transactional
+    void save(){
+        Teacher teacher = new Teacher();
+        int sizeBeforeSaving = teacherRepository.findAllTeachers().size();
+
+        teacher = teacherRepository.addTeacher(teacher);
+
+        assertThat(teacherRepository.listAll().size()).isEqualTo(sizeBeforeSaving + 1);
+        assertThat(teacherRepository.listAll()).contains(teacher);
+
+
+    }
+}

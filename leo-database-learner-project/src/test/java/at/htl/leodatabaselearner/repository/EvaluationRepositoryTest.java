bfaa39fdc2a476c81ffb7516664ce6256223654d
Marah Steigersdorfer <madschi.steigersdorfer@gmx.at>
2021-12-03 11:34:07 +0100

    updated EvaluationRepositoryTest - updated save() #77

--- leo-database-learner-project/src/test/java/at/htl/leodatabaselearner/repository/EvaluationRepositoryTest.java
+++ leo-database-learner-project/src/test/java/at/htl/leodatabaselearner/repository/EvaluationRepositoryTest.java
@@ -1,9 +1,6 @@
 package at.htl.leodatabaselearner.repository;
 
-import at.htl.leodatabaselearner.entity.Evaluation;
-import at.htl.leodatabaselearner.entity.ExercisePackage;
-import at.htl.leodatabaselearner.entity.Question;
-import at.htl.leodatabaselearner.entity.Student;
+import at.htl.leodatabaselearner.entity.*;
 import io.quarkus.test.junit.QuarkusTest;
 import org.junit.jupiter.api.Test;
 
@@ -23,7 +20,9 @@ class EvaluationRepositoryTest {
     @Transactional
     void save(){
         Student student = new Student("Franz","Huber","passme123","5AHITM");
-        ExercisePackage exercisePackage = new ExercisePackage();
+        Teacher teacher = new Teacher("Fritz", "Fliege", "test1234");
+        DataModel dataModel = new DataModel("Leodatabaselearner", teacher, "comment");
+        ExercisePackage exercisePackage = new ExercisePackage("ExercisePackage", dataModel);
         Question question = new Question("","","",2, exercisePackage);
         Evaluation evaluation = new Evaluation(10, student, question);
 

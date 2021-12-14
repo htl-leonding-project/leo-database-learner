ea418f487a1e2d6a8fddfec57cc12b3a9a1f296f
turnerisabel <turner.isabel03@gmail.com>
2021-12-14 10:21:41 +0100

    #102 updated person.ts

--- leo-database-learner-frontend/src/app/models/person.ts
+++ leo-database-learner-frontend/src/app/models/person.ts
@@ -3,11 +3,13 @@ export class Person {
   id: number;
   firstName: string;
   lastName: string;
+  password: string
 
 
-  constructor(id: number, firstName: string, lastName: string) {
+  constructor(id: number, firstName: string, lastName: string, password: string) {
     this.id = id;
     this.firstName = firstName;
     this.lastName = lastName;
+    this.password = password;
   }
 }
